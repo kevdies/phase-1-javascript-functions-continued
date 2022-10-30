2155bb7707c2aeb7329988b87318db6ee6bf928a
kevin diesenberg <kdiesenb@gmail.com>
2022-10-30 19:42:35 -0400

    commit tree b8495783d0cdbefac19c2a214c4843ed1ab0f4ec

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    switch("activity") {
+        case console.log(`This Saturday, I want to ${activity}!`):
+            break;
+        default:
+            return `This Saturday, I want to ${activity}!`;
+    }
+};
+
+const mondayWork = function (activity = "go to the office") {
+    switch("activity") {
+        case console.log(`This Monday, I will ${activity}.`):
+            break;
+            default: return `This Monday, I will ${activity}.`;
+    }
+}
+
+const wrapAdjective = function(parameter) {
+    return function(word = "special") {
+        return `You are ${parameter}${word}${parameter}!`;
+    }
+}
